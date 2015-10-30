2e01906de464e17afd7b7ad3d902ee98a6932eeb
Junior <Junior@Junior-PC>
2015-10-30 19:21:45 -0200

    Criada a classe para fazer conexão com o Banco de dados.

---
+++ src/main/java/br/univel/util/AtualizadorBancoDados.java
@@ -0,0 +1,13 @@
+package br.univel.util;
+
+public class AtualizadorBancoDados {
+
+	public AtualizadorBancoDados() {
+		abrirConexaoBancoDados();
+	}
+
+	private void abrirConexaoBancoDados() {
+
+	}
+
+}
