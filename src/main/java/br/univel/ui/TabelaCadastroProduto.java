696260adea9eed42475d044f5a35c427f16b698b
Junior <Junior@Junior-PC>
2015-11-03 19:21:05 -0200

    Criado métodos para dar valor e nome as colunas da tabela de cadastro de Produto.

--- src/main/java/br/univel/ui/TabelaCadastroProduto.java
+++ src/main/java/br/univel/ui/TabelaCadastroProduto.java
@@ -1,6 +1,11 @@
 package br.univel.ui;
 
+import java.util.ArrayList;
+
 import javax.swing.table.AbstractTableModel;
+
+import br.univel.util.Cliente;
+import br.univel.util.Produto;
 
 /**
  * 
@@ -10,19 +15,76 @@ import javax.swing.table.AbstractTableModel;
 
 public class TabelaCadastroProduto extends AbstractTableModel {
 
+	private ArrayList<Produto> lista = new ArrayList<Produto>();
+
+	public ArrayList<Produto> getLista() {
+		return lista;
+	}
+
+	public void setLista(ArrayList<Produto> lista) {
+		this.lista = (ArrayList<Produto>) lista;
+		this.fireTableDataChanged();
+	}
+
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return lista.size();
 	}
 
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 8;
 	}
 
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Método para dar valor as colunas
+	 */
+	public Object getValueAt(int row, int column) {
+		Produto produto = lista.get(row);
+		switch (column) {
+		case 0:
+			return produto.getId();
+		case 1:
+			return produto.getNome();
+		case 2:
+			return produto.getCodigoBarra();
+		case 3:
+			return produto.getCategoria();
+		case 4:
+			return produto.getDescricao();
+		case 5:
+			return produto.getUnidade();
+		case 6:
+			return produto.getCusto();
+		case 7:
+			return produto.getMargemLucro();
+		}
+		return "Erro";
+	}
+
+	/**
+	 * Método para dar nome as colunas
+	 */
+
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0:
+			return "Id";
+		case 1:
+			return "Nome";
+		case 2:
+			return "Código de Barra";
+		case 3:
+			return "Categoria";
+		case 4:
+			return "Descrição";
+		case 5:
+			return "Unidade";
+		case 6:
+			return "Custo";
+		case 7:
+			return "Margem de Lucro";
+		}
+		return "Erro";
 	}
 
 }
