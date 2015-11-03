00959124771bb3b5a90f91e32550a10a82d2ea2a
Junior <Junior@Junior-PC>
2015-11-03 19:34:21 -0200

    Criado método para adicionar valores da Enum Categoria no ComboBox.

--- src/main/java/br/univel/ui/MioloCadastroProduto.java
+++ src/main/java/br/univel/ui/MioloCadastroProduto.java
@@ -1,10 +1,16 @@
 package br.univel.ui;
 
 import javax.swing.JPanel;
+
 import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
+
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
@@ -14,6 +20,12 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
+import br.univel.util.Categoria;
+import br.univel.util.Estado;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MioloCadastroProduto extends JPanel {
 	private JTextField textField;
 	private JTextField textField_1;
@@ -22,18 +34,24 @@ public class MioloCadastroProduto extends JPanel {
 	private JTextField textField_6;
 	private JTextField textField_7;
 	private JTable table;
+	private TabelaCadastroProduto model = new TabelaCadastroProduto();
+	private JComboBox cbxCategoria;
+	private JComboBox cbxUnidade;
 
 	/**
 	 * @author João Carlos Barbosa Júnior 29/10/2015 19:31:12
 	 */
 	public MioloCadastroProduto() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{82, 173, 89, 89, 0};
-		gridBagLayout.rowHeights = new int[]{20, 20, 20, 20, 20, 20, 20, 20, 23, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 82, 173, 89, 89, 0 };
+		gridBagLayout.rowHeights = new int[] { 20, 20, 20, 20, 20, 20, 20, 20,
+				23, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0,
+				Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		JLabel lblNewLabel = new JLabel("Id");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
@@ -41,7 +59,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 0;
 		add(lblNewLabel, gbc_lblNewLabel);
-		
+
 		textField = new JTextField();
 		textField.setColumns(10);
 		GridBagConstraints gbc_textField = new GridBagConstraints();
@@ -52,7 +70,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_textField.gridx = 1;
 		gbc_textField.gridy = 0;
 		add(textField, gbc_textField);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Nome");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
@@ -60,7 +78,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 1;
 		add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
@@ -71,7 +89,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_textField_1.gridx = 1;
 		gbc_textField_1.gridy = 1;
 		add(textField_1, gbc_textField_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("C\u00F3digo de Barra");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
@@ -79,7 +97,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_2.gridx = 0;
 		gbc_lblNewLabel_2.gridy = 2;
 		add(lblNewLabel_2, gbc_lblNewLabel_2);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
@@ -90,7 +108,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_textField_2.gridx = 1;
 		gbc_textField_2.gridy = 2;
 		add(textField_2, gbc_textField_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Categoria");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
@@ -98,16 +116,16 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_3.gridx = 0;
 		gbc_lblNewLabel_3.gridy = 3;
 		add(lblNewLabel_3, gbc_lblNewLabel_3);
-		
-		JComboBox comboBox = new JComboBox();
+
+		cbxCategoria = new JComboBox();
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
 		gbc_comboBox.gridwidth = 3;
 		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox.gridx = 1;
 		gbc_comboBox.gridy = 3;
-		add(comboBox, gbc_comboBox);
-		
+		add(cbxCategoria, gbc_comboBox);
+
 		JLabel lblNewLabel_4 = new JLabel("Descri\u00E7\u00E3o");
 		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
 		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
@@ -115,7 +133,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_4.gridx = 0;
 		gbc_lblNewLabel_4.gridy = 4;
 		add(lblNewLabel_4, gbc_lblNewLabel_4);
-		
+
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
 		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
@@ -126,7 +144,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_textField_4.gridx = 1;
 		gbc_textField_4.gridy = 4;
 		add(textField_4, gbc_textField_4);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("Unidade");
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
 		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
@@ -134,16 +152,16 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_5.gridx = 0;
 		gbc_lblNewLabel_5.gridy = 5;
 		add(lblNewLabel_5, gbc_lblNewLabel_5);
-		
-		JComboBox comboBox_1 = new JComboBox();
+
+		cbxUnidade = new JComboBox();
 		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
 		gbc_comboBox_1.gridwidth = 3;
 		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
 		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox_1.gridx = 1;
 		gbc_comboBox_1.gridy = 5;
-		add(comboBox_1, gbc_comboBox_1);
-		
+		add(cbxUnidade, gbc_comboBox_1);
+
 		JLabel lblNewLabel_6 = new JLabel("Custo");
 		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
 		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
@@ -151,7 +169,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_6.gridx = 0;
 		gbc_lblNewLabel_6.gridy = 6;
 		add(lblNewLabel_6, gbc_lblNewLabel_6);
-		
+
 		textField_6 = new JTextField();
 		textField_6.setColumns(10);
 		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
@@ -162,7 +180,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_textField_6.gridx = 1;
 		gbc_textField_6.gridy = 6;
 		add(textField_6, gbc_textField_6);
-		
+
 		JLabel lblNewLabel_7 = new JLabel("Margem de Lucro");
 		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
 		gbc_lblNewLabel_7.anchor = GridBagConstraints.WEST;
@@ -170,7 +188,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_lblNewLabel_7.gridx = 0;
 		gbc_lblNewLabel_7.gridy = 7;
 		add(lblNewLabel_7, gbc_lblNewLabel_7);
-		
+
 		textField_7 = new JTextField();
 		textField_7.setColumns(10);
 		GridBagConstraints gbc_textField_7 = new GridBagConstraints();
@@ -181,7 +199,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_textField_7.gridx = 1;
 		gbc_textField_7.gridy = 7;
 		add(textField_7, gbc_textField_7);
-		
+
 		JButton btnNewButton_2 = new JButton("Alterar");
 		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
 		gbc_btnNewButton_2.anchor = GridBagConstraints.NORTHEAST;
@@ -189,7 +207,7 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_btnNewButton_2.gridx = 1;
 		gbc_btnNewButton_2.gridy = 8;
 		add(btnNewButton_2, gbc_btnNewButton_2);
-		
+
 		JButton btnNewButton_1 = new JButton("Excluir");
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
 		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTH;
@@ -197,15 +215,20 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_btnNewButton_1.gridx = 2;
 		gbc_btnNewButton_1.gridy = 8;
 		add(btnNewButton_1, gbc_btnNewButton_1);
-		
+
 		JButton btnNewButton = new JButton("Gravar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gravaProdutoBanco();
+			}
+		});
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton.anchor = GridBagConstraints.NORTH;
 		gbc_btnNewButton.gridx = 3;
 		gbc_btnNewButton.gridy = 8;
 		add(btnNewButton, gbc_btnNewButton);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.gridwidth = 4;
@@ -213,9 +236,24 @@ public class MioloCadastroProduto extends JPanel {
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 9;
 		add(scrollPane, gbc_scrollPane);
-		
-		table = new JTable();
+
+		table = new JTable(model);
 		scrollPane.setViewportView(table);
+		
+		adicionarEnumComboBoxCategoria();
+
+	}
+
+	/**
+	 * Método para adicionar valores da Enum Categoria no comboBox
+	 */
+	private void adicionarEnumComboBoxCategoria() {
+		Categoria[] categorias = Categoria.values();
+		ComboBoxModel cbxModelCategoria = new DefaultComboBoxModel(categorias);
+		cbxCategoria.setModel(cbxModelCategoria);
+	}
+
+	protected void gravaProdutoBanco() {
 
 	}
 }
