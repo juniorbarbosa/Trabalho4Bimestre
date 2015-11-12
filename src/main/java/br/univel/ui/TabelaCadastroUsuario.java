8c9d9cc7d9fa2f809e784c6b19aeffc9e0bf9bf8
Junior <Junior@Junior-PC>
2015-11-12 19:21:26 -0200

    Criada tabela para tela de cadastro de usuário.

---
+++ src/main/java/br/univel/ui/TabelaCadastroUsuario.java
@@ -0,0 +1,44 @@
+package br.univel.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import br.univel.util.Usuario;
+
+/**
+ * 
+ * @author João Carlos Barbosa Júnior 12/11/2015 19:17:41
+ *
+ */
+
+public class TabelaCadastroUsuario extends AbstractTableModel {
+
+	/**
+	 * criada lista para poder utilizar todos os dados do cliente na tabela, com
+	 * get e set.
+	 */
+	private ArrayList<Usuario> lista = new ArrayList<Usuario>();
+
+	public ArrayList<Usuario> getLista() {
+		return lista;
+	}
+
+	public void setLista(ArrayList<Usuario> lista) {
+		this.lista = lista;
+	}
+
+	public int getRowCount() {
+		return 0;
+	}
+
+	public int getColumnCount() {
+		return 0;
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		return null;
+	}
+
+}
